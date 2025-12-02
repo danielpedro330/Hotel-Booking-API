09cbf774297295f5bddc8345424ae901b1d85777
danielpedro330 <dp4144036@gmail.com>
2025-12-02 15:49:19 +0100

    chore(env): allow missing env vars during unit tests

--- src/env/index.ts
+++ src/env/index.ts
@@ -17,10 +17,11 @@ const envSchema = z.object({
 })
 
 const _env = envSchema.safeParse(process.env)
-if (_env.success == false) {
-    console.error('Invalid environment variable', _env.error.format())
-
-    throw new Error('Invalid environment variable')
+if (_env.error) {
+  if (process.env.NODE_ENV !== "test") {
+    console.error("Invalid environment variable", _env.error.format());
+    throw new Error("Invalid environment variable");
+  }
 }
 
 export const env = _env.data
